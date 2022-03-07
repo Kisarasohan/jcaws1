e1f53269a1e2f5d1ee90c30618ecb9fcac47958a
Kisarasohan <98421810+Kisarasohan@users.noreply.github.com>
2022-03-07 16:51:54 +0530

    Update PassValidatorTest.java

--- src/test/java/PassValidatorTest.java
+++ src/test/java/PassValidatorTest.java
@@ -7,7 +7,7 @@ public class PassValidatorTest {
 	@Test
 	public void test() {
 		//PassValidator ob=new PassValidator();
-		boolean res=PassValidator.isValid("ab1234567899999");
+		boolean res=PassValidator.isValid("a");
 		assertEquals(true,res);
 	}
 
